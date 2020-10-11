887621873473277a07bc38c97e1f0a555764b015
dfrias100 <dfrias100@tamu.edu>
2020-10-11 15:16:18 -0500

    holy shit the syncrhonization works

--- pcbuffer.cpp
+++ pcbuffer.cpp
@@ -49,10 +49,13 @@
 /* FUNCTIONS FOR CLASS P C B u f f e r */
 /*--------------------------------------------------------------------------*/
 
-PCBuffer::PCBuffer(int _size) : size(_size) {
-    m = PTHREAD_MUTEX_INITIALIZER;    
-    notfull = PTHREAD_COND_INITIALIZER;
-    notempty = PTHREAD_COND_INITIALIZER;
+PCBuffer::PCBuffer(int _size) : size(_size), empty(_size), full(0), mutex(1) {
+    //m = PTHREAD_MUTEX_INITIALIZER;
+    //notfull = PTHREAD_COND_INITIALIZER;
+    //notempty = PTHREAD_COND_INITIALIZER;
+    //empty = Semaphore(size);
+    //full = Semaphore(0);
+    //mutex = Semaphore(1);
     buffer = new string[size];
     nextin = 0;
     nextout = 0;
@@ -64,26 +67,34 @@ PCBuffer::~PCBuffer() {
 }
 
 int PCBuffer::Deposit(string _item) {
-    pthread_mutex_lock(&m);
-    while (count == size)
-        pthread_cond_wait(&notfull, &m);
+    //pthread_mutex_lock(&m);
+    //while (count == size)
+    //    pthread_cond_wait(&notempty, &m);
+    empty.P();
+    mutex.P();
     buffer[nextin] = _item;
     nextin = (nextin + 1) % size;
     count = count + 1;
-    pthread_cond_signal(&notempty);
-    pthread_mutex_unlock(&m);
+    mutex.V();
+    full.V();
+    //pthread_mutex_unlock(&m);
+    //pthread_cond_signal(&notfull);
     return count;
 }
 
 
 string PCBuffer::Retrieve() {
-    pthread_mutex_lock(&m);
-    while (count == 0)
-        pthread_cond_wait(&notempty, &m);
+    //while (count == 0)
+    //    pthread_cond_wait(&notempty, &m);
+    //pthread_mutex_lock(&m);
+    full.P();
+    mutex.P();
     string ret = buffer[nextout];
     nextout = (nextout + 1) % size;
     count = count - 1;
-    pthread_cond_signal(&notfull);
-    pthread_mutex_unlock(&m);
+    mutex.V();
+    empty.V();
+    //pthread_mutex_unlock(&m);
+    //pthread_cond_signal(&notfull);
     return ret;
 }
